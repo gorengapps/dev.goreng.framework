fd9b2fb2aae13e271eb2eaeba5d693823a19abbf
Jason Meulenhoff <jason@dxrs.nl>
2024-04-08 10:27:58 +0200

    Add custom editor that automates some error prone configuration

--- Runtime/Scene/Editor/AsyncSceneEditor.cs
+++ Runtime/Scene/Editor/AsyncSceneEditor.cs
@@ -1,7 +1,75 @@
+#if UNITY_EDITOR
+
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
+using UnityEngine.UIElements;
+
 namespace Frame.Runtime.Scene.Editor
 {
-    public class AsyncSceneEditor
+    [CustomEditor(typeof(AsyncScene), true)]
+    public class AsyncSceneEditor : UnityEditor.Editor
     {
+        private void UpdateAddressableLabel()
+        {
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
+            var path = AssetDatabase.GetAssetPath(target);
+            
+            var assetGuid = AssetDatabase.AssetPathToGUID(path);
+            
+
+            // Addressable was not created for this item
+            if (settings.FindAssetEntry(assetGuid) == null)
+            {
+                settings.CreateAssetReference(assetGuid);    
+            }
+            
+            var entry = settings.FindAssetEntry(assetGuid);
+
+            if (entry.address != path)
+            {
+                entry.SetAddress(path);
+            }
+            
+            // /entry.SetAddress();
+            entry.SetLabel("scenes", true, true, false);
+
+        }
         
+        public override VisualElement CreateInspectorGUI()
+        {
+            UpdateAddressableLabel();
+            return base.CreateInspectorGUI();
+        }
+        
+        [MenuItem("Assets/Create/Framework/Scene/Create Scene", false, 1)]
+        public static void CreateScene()
+        {
+            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+            if (string.IsNullOrEmpty(path))
+            {
+                path = "Assets/";
+            }
+            
+            var asset = CreateInstance<AsyncScene>();
+            
+            AssetDatabase.CreateAsset(asset, path + "/Scene.asset"); 
+            EditorUtility.SetDirty(asset);
+            
+            AssetDatabase.SaveAssets();
+            
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
+            var assetPath = AssetDatabase.GetAssetPath(asset);
+            var assetGuid = AssetDatabase.AssetPathToGUID(assetPath);
+            
+            settings.CreateAssetReference(assetGuid);
+            Selection.activeObject = asset;
+        } 
     }
 }
+
+#endif
